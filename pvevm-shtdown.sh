ae9841d6e68b6e877d467a0dc072f3e4e5af11b3
tehlocust <104439805+tehlocust@users.noreply.github.com>
2022-04-26 21:41:49 +0200

    Create pvevm-shtdown.sh
    # script to gracefully shutdown the proxmox virtual machines
    # need to change it to automatically detect their vm id's

---
+++ pvevm-shtdown.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# script to gracefully shutdown the proxmox virtual machines
+# need to change it to automatically detect their vm id's
+
+echo
+echo "##### Shutting down virtual machines... #####"
+echo
+
+for vmn in {100..105}; do
+	qm shutdown ${vmn}
+	if [[ ${@} -eq 0 ]]; then
+    echo
+		echo "##### Shutdown sent successfully #####"
+    echo
+	else
+    echo
+		echo "##### Possible shutdown adventures... #####"
+    echo
+	fi
+done
+
+echo
+echo "##### Finishing... bye #####"
+echo
